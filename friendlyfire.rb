f35e6fc9485886aa6b031c2cf82cd740f4627eea
Matthew Bunday <zenceph@gmail.com>
2014-05-29 16:56:32 -0400

    Clean up file.

--- friendlyfire.rb
+++ friendlyfire.rb
@@ -1,36 +1,20 @@
 require 'colored'
 require 'open3'
 
-#puts ARGV
 cmd = "ruby #{ARGV[0]}"
-#puts output
-#puts $?.to_i
 
 output = ""
 Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
   output << stderr.read
 end
 
-#puts output
-
 # error status returned
 if output
   lines = output.lines
+
+  # Future, red the first line originating from a file in the same directory, ie. not in a gem
   puts lines.shift.chomp.red
   lines.each do |line|
     puts line
   end
 end
-
-#input = "begin\n"
-#
-#ARGF.each do |line|
-#  input << line
-#end
-#
-#input << "rescue Exception => e\n"
-#input << "puts e.message\n"
-#input << "puts e.backtrace\n"
-#input << "end"
-#
-#eval(input)
