5e4e379942ad6d7c71bbbbb70e101551bcd94e82
Matthew Bunday <zenceph@gmail.com>
2014-05-29 16:37:21 -0400

    Use piping so the stack trace will make sense.

--- friendlyfire.rb
+++ friendlyfire.rb
@@ -1,11 +1,20 @@
-input = "begin\n"
+output = `ruby #{ARGV[0]}`
+puts output
 
-ARGF.each do |line|
-  input << line
+# error status returned
+if $?.to_i == 1
+  # analyze output
 end
 
-input << "rescue\n"
-input << "puts 'hello'\n"
-input << "end"
-
-eval(input)
+#input = "begin\n"
+#
+#ARGF.each do |line|
+#  input << line
+#end
+#
+#input << "rescue Exception => e\n"
+#input << "puts e.message\n"
+#input << "puts e.backtrace\n"
+#input << "end"
+#
+#eval(input)
